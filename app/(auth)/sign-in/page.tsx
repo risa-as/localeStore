820d80f937ab2a195ddb30cdf4477e1270c85899
Ayad Qaid <ayadqaid12345@gmail.com>
2025-12-15 11:32:54 +0300

    Initial commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -17,7 +17,7 @@ const SignInPage = async (props: {
 }) => {
     const {callbackUrl} = await props.searchParams;
     const session = await auth()
-    if(session) {
+    if(session?.user?.id) {
         return redirect(callbackUrl || "/")
     }
     return ( 
